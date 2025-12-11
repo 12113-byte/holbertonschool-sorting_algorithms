c6f90b9b59e2957a4601a0b0c68dce67e8fd5f1e
Joleen <12113@holbertonstudents.com>
2025-12-11 03:56:51 +0000

    sorting algorithms and big o

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the bubble sort algorithm
+ * @array: the array that has to be sorted
+ * @size: of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[j] > array[j + 1])
+		{
+			temp = array[j + 1];
+			array[j + 1] = array[i];
+			array[j] = temp;
+			printf("%d", array);
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+Tasks about sorting algorithms
+
+Task 0 - sorts an array of integers in ascending order using the Bubble sort algorithmm
+	file 0-O: big O notations of the time complexity in best, average and worst case
+Task 1 - sorts a doubly linked list of integers in ascending order usding the insertion sort algorithm
+Task 2 - sorts an array of integers in ascending order using the selectioon sort algorithm
+Task 3 - sorts an array of integers in ascending order using the quick sort algorithm
