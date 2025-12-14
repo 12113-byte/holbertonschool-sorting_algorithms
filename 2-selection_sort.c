ed770a888faed13ae4feeed3e228349e311a2577
Joleen <12113@holbertonstudents.com>
2025-12-14 11:51:28 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the selection sort algorithm
+ * @array: pointer to array
+ * @size: of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	if (!array || size < 2)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j ++) /*finding smallest number*/
+		{
+			if (array[j] < array [min_index])
+			{
+				min_index = j;
+			}
+		}
+
+		if (min_index != i) /*swap if needed*/
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
+
